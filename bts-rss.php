e36e514c96e23805096e400e9744889aa5da56c7
psalmenarle <60122554+psalmenarle@users.noreply.github.com>
2020-01-27 19:59:21 +0800

    Create bts-rss.php

---
+++ bts-rss.php
@@ -0,0 +1,22 @@
+<?php
+ $domOBJ = new DOMDocument();
+ $domOBJ->load("btsprofile.xml");  
+ 
+ $content = $domOBJ->getElementsByTagName("item");
+ 
+ ?>
+ <ul>
+    <?php
+ foreach( $content as $data )
+ {
+   $title = $data->getElementsByTagName("title")->item(0)->nodeValue;
+   $link = $data->getElementsByTagName("link")->item(0)->nodeValue;
+  
+   echo "<li>$title
+            <ul>
+                <li>$link</li>
+            </ul>
+        </li>";
+ }
+?>
+</ul>
